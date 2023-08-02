3669187ab5df490bb9cc98d42ac078fe8eca1d3b
Jigsaw578 <quocthinh.11ly.chl@gmail.com>
2023-08-02 14:45:55 +0700

    ProductPage

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Home } from "../pages/Home/Home";
-import { Product } from "../pages/Product/Product";
+import { Products } from "../pages/Product/Product";
 
 export const AppRoute: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/product" element={<Product />} />
+      <Route path="/products" element={<Products />} />
     </Routes>
   );
 };
